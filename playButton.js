097b2d734e99df31123895dfb0beed7cbd804968
Andrew Jones <andrewjones@Andrews-MacBook-Pro.local>
2016-06-13 22:22:42 -0400

    first song played by button

---
+++ playButton.js
@@ -0,0 +1,15 @@
+
+$('#push').click(function() {
+	console.log('pushed');
+	$.ajax({
+		url: 'https://api.spotify.com/v1/tracks/3BxWKCI06eQ5Od8TY2JBeA',
+		id: '3BxWKCI06eQ5Od8TY2JBeA',
+		success: function(response) {
+			console.log(response);
+			var track = response.preview_url;
+			var audio = new Audio();
+			audio.src = track;
+			audio.play();
+		}
+	})
+});
