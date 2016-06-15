eea2c0dcdde2062c55015516a94cc196087afbe9
Andrew Jones <andrewjones@Andrews-MacBook-Pro.local>
2016-06-14 23:27:24 -0400

    added some animations

--- js/playButton.js
+++ js/playButton.js
@@ -1,6 +1,5 @@
 
 $('#push').click(function() {
-	console.log('pushed');
 	$.ajax({
 		url: 'https://api.spotify.com/v1/tracks/3BxWKCI06eQ5Od8TY2JBeA',
 		id: '3BxWKCI06eQ5Od8TY2JBeA',
@@ -10,6 +9,27 @@ $('#push').click(function() {
 			var audio = new Audio();
 			audio.src = track;
 			audio.play();
+			setTimeout(audio.pause(), 2);
 		}
-	})
+	});
 });
+
+$(document).ready(function() {
+	$('#push').click(function() {
+		$('#push').animate({
+			left: "200px"
+		});
+	});
+});
+
+function randomDir() {
+	var height = $(window).height();
+	var width = $(window).width();
+
+	var randDecVert = Math.random();
+	var randDecHorz = Math.random();
+	randVert = randDecVert * height;
+	randHorz = randDecHorz * width;
+
+	return [randVert, randHorz];
+}
